5be83c282c305c2bd17c608523181404f33b9315
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-05-12 10:19:13 -0400

    fleshed out the mai npage

--- app/locales/en.js
+++ app/locales/en.js
@@ -3,8 +3,38 @@ export default{
   subheader: 'Event registration designed for dancers.',
   lookingforevent: 'Looking for an event?',
   hostinganevent: 'Hosting an event?',
+  lookingforscene: 'Looking for a scene?',
+  whatisheader: 'What is ÆONVERA?',
+  whatis: 'It is a system that helps event and scene organizers manage their events, lessons, and dances. \
+    It helps scenes manage memberships and benefits. \
+    It helps event organizers more easily manage their event both before and during the event. \
+    It is constantly growing and responding to the community and doing everything it can to make the lives of organizers easier. \
+    For more information on "how", checkout the features page.',
+
+  features: 'Features',
+  pricing: 'Pricing',
+  faq: 'F.A.Q.',
+
+  featuresinfo: 'List of current and upcoming features and possible explanations of each. \
+    ÆONVERA is written by, and is for Swing Dancers.',
+  pricinginfo: 'Overview of pricing. \
+    The most important thing to note is that if your event dosn\'t have a need \
+    for collecting payments electronically, you can use ÆONVERA for free. \
+    Every feature.',
+  faqinfo: 'Questions that are frequently asked as well as ones that might be \
+    thought, but not spoken.',
+
+
   tos: 'Terms of Service',
   privacy: 'Privacy',
   submitideas: 'Submit Ideas',
-  copyright: 'Copyright © 2013-2015 Precognition LLC'
+  copyright: 'Copyright © 2013-2015 Precognition LLC',
+
+  buttons: {
+    login: 'Login',
+    signup: 'Sign Up',
+    eventcalendar: 'Event Calendar',
+    createyourevent: 'Create Your Event',
+    scenesbycity: 'Scenes By City'
+  }
 };
