8d4d2dafa58189983bbe78d5d347aa94fbd5f1be
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-05-15 09:39:43 -0400

    Progress

--- app/components/fixed-top-nav.js
+++ app/components/fixed-top-nav.js
@@ -1,14 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'div',
-  classNames: ['fixed'],
+	tagName: 'div',
+	classNames: ['fixed'],
 
-  backLinkPath: function() {
-    return 'welcome';
-  }.property(),
+	backLinkPath: function() {
+		return 'welcome';
+	}.property(),
 
-  backLinkText: function() {
-    return this.t('appname');
-  }.property()
+	backLinkText: function() {
+		return this.t('appname');
+	}.property(),
+
+	hasLeftMobileMenu: function() {
+		return !Ember.isEmpty(this.get('left'));
+	}.property(),
+
+	hasRightMobileMenu: function() {
+		return !Ember.isEmpty(this.get('right'));
+	}.property()
 });
--- app/controllers/welcome.js
+++ app/controllers/welcome.js
@@ -1,14 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	needs: ['application'],
-
-	application: Ember.computed.alias("controllers.application"),
-
-	hasLeftMobileMenu: function() {
-		console.log(this.application.get('hasLeftMobileMenu'));
-		this.application.get('hasLeftMobileMenu');
-	}.property()
 
 
 });
