e5719c97a3fb5f1a45eadb80a2b1200c6679086d
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-05-15 11:17:43 -0400

    changed nav menu to component instead of template

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-
+  navigation: 'nav/welcome/top-menu'
 
 });
--- app/routes/welcome.js
+++ app/routes/welcome.js
@@ -2,29 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  setupController: function(controller, model) {
-    this.controller.send('setMobileMenuProperties');
-    this._super(controller, model);
-  },
+	setupController: function(controller, model) {
+		this.controller.send('setMobileMenuProperties');
+		this._super(controller, model);
+	},
 
 
-  // http://stackoverflow.com/questions/12150624/ember-js-multiple-named-outlet-usage
-  renderTemplate: function() {
+	// http://stackoverflow.com/questions/12150624/ember-js-multiple-named-outlet-usage
+	renderTemplate: function() {
 
-    // Render default outlet
-    this.render();
+		// Render default outlet
+		this.render();
 
-    // render right nav items
-    this.render('welcome/menu/menu', {
-      outlet: 'fixed-top-nav',
-      into: 'application'
-    });
+		// render footer
+		this.render('shared/footer', {
+			outlet: 'bottom-footer',
+			into: 'application'
+		});
 
-    // render footer
-    this.render('shared/footer', {
-      outlet: 'bottom-footer',
-      into: 'application'
-    });
-
-  }
+	}
 });
