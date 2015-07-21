53611858f4e21c2905a9638e6c89bac8d9f2c9f8
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-07-20 21:59:46 -0400

    address some stylings

--- app/models/upcoming-event.js
+++ app/models/upcoming-event.js
@@ -6,5 +6,11 @@ export default DS.Model.extend({
   startsAt: DS.attr('date'),
   endsAt: DS.attr('date'),
   location: DS.attr('string'),
-  url: DS.attr('string')
+  url: DS.attr('string'),
+
+  isRegistrationOpen: function() {
+    var opensAt = this.get('registrationOpensAt').getTime();
+    var now = Date.now;
+    return (now > opensAt);
+  }.property('registrationOpensAt')
 });
