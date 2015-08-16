b5e72417dbb84b94747a6de0d119a442949adadc
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-08-16 19:39:06 -0400

    revert adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
   namespace: 'api',
-  // host: 'https://aeonvera.com'
-  host: 'http://swing.vhost:3000'
+  host: 'https://aeonvera.com'
+  // host: 'http://swing.vhost:3000'
 });
