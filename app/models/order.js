9666527edaa296db4594480689b523d46797f41d
NullVoxPopuli <LPSego3+dev@gmail.com>
2015-08-19 19:59:56 -0400

    finished orders

--- app/models/order.js
+++ app/models/order.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   hostName: DS.attr('string'),
+  hostUrl: DS.attr('string'),
   createdAt: DS.attr('date'),
   paidAmount: DS.attr('number'),
   netAmountReceived: DS.attr('number'),
