cc30d06cefca17f0326a204d87c9c5be116d66d6
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-10-07 15:57:15 -0400

    prep for refactoring login so it can be in a modal and not

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
-  openLogin: function(){
-    Ember.$(document).foundation('reflow');
-
-    Ember.$('#login-modal').foundation('reveal', 'open');
-    // Ember.$('a[data-reveal-id="login-modal"]').click();
-  }.on('didInsertElement')
+  actions: {
+    authenticate: function() {
+      var data = this.getProperties('identification', 'password');
+      return this.get('session').authenticate('authenticator:devise', data);
+    }
+  }
 });
