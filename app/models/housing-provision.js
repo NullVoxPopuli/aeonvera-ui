afbf761e74c60c8d3cdecb80fc32d06bc6cf7d40
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-11-01 12:10:05 -0500

    progress on housing displaying

---
+++ app/models/housing-provision.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+import PaymentStatus from '../mixins/models/payment-status';
+
+export default DS.Model.extend({
+  housingCapacity: DS.attr('number'),
+  numberOfShowers: DS.attr('number'),
+  canProvideTransportation: DS.attr('boolean'),
+  transportationCapacity: DS.attr('number'),
+  preferredGenderToHost: DS.attr('string'),
+  hasPets: DS.attr('boolean'),
+  smokes: DS.attr('boolean'),
+  notes: DS.attr('string'),
+
+  host: DS.belongsTo('host', { polymorphic: true} ),
+  attendance: DS.belongsTo('attendance', { polymorphic: true } )
+
+
+});
---
+++ app/models/housing-request.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  needTransportation: DS.attr('boolean'),
+  canProvideTransportation: DS.attr('boolean'),
+  allergicToPets: DS.attr('boolean'),
+  allergicToSmoke: DS.attr('boolean'),
+  otherAllergies: DS.attr('string'),
+  requestedRoommates: DS.attr(),
+  unwantedRoommates: DS.attr(),
+  preferredGenderToHouseWith: DS.attr('string'),
+  notes: DS.attr('string'),
+
+  host: DS.belongsTo('host', { polymorphic: true} ),
+  attendance: DS.belongsTo('attendance', { polymorphic: true } ),
+  housingProvision: DS.belongsTo('housing-provision')
+
+});
