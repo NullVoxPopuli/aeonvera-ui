fc4dbe9eae7704a551e086b73da7e48af5bd4577
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-12-08 10:21:32 -0500

    fix issue with hyphenations

--- app/mixins/routes/crud/events/show.js
+++ app/mixins/routes/crud/events/show.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   model: function(params) {
     let modelName = this.get('modelName');
-    let idName = modelName + '_id';
+    let idName = modelName.underscore() + '_id';
     return this.store.findRecord(modelName, params[idName]);
   }
 });
