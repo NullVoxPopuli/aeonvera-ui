23911266f44eafbca616f2de465d23a0d005409a
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-01-05 10:44:49 -0500

    woo tired

--- app/routes/my-communities/index.js
+++ app/routes/my-communities/index.js
@@ -1,12 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // session: Ember.inject.service('session'),
 
   model: function() {
-    let userId = this.get('currentUser.user.id');
     return this.store.query('organization', {
-      ownerId: userId
-    })
+      mine: true
+    });
   }
 });
