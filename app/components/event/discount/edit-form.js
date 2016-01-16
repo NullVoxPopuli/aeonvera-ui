1e1a9346ba242ea54d2c1704f01c4f7125aae9cc
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-01-16 13:39:11 -0500

    upgrade ember and ember data, fix discount cancel path

--- app/components/event/discount/edit-form.js
+++ app/components/event/discount/edit-form.js
@@ -4,7 +4,7 @@ import Form from 'aeonvera/mixins/components/edit-form';
 export default Ember.Component.extend(Form, {
   modelName: 'discount',
   saveSuccessPath: 'events.show.discounts', // should be show?
-  cancelPath: 'events.show.discount',
+  cancelPath: 'events.show.discounts',
   parentAssociation: 'event',
   parentId: 'event_id'
 });
