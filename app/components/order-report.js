dd6a8492b5a111c7d3d9aa1896cd82db372c484f
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-02-06 20:03:25 -0500

    yay sorting!

--- app/components/order-report.js
+++ app/components/order-report.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
   afterDate: null,
   beforeDate: null,
   pastDays: 35,
   firstOrLastNameContains: null,
   // 0 - all, 1 - paid, 2 - unpaid
   showPaid: 0,
+
+  sortedOrders: Ember.computed.sort('orders', 'sortProps'),
+  sortProps: ['paymentReceivedAt:desc'],
 
   orders: function() {
     let host = this.get('model');
@@ -28,8 +30,8 @@ export default Ember.Component.extend({
     }
 
     if (Ember.isPresent(nameContains)) {
-      //query['user_first_name_or_user_last_name_cont'] = nameContains;
-      query['attendance_attendee_name_cont'] = nameContains;
+      query['user_first_name_or_user_last_name_cont'] = nameContains;
+      // query['attendance_attendee_name_cont'] = nameContains;
     }
 
     if (showPaid != 0) {
@@ -46,8 +48,62 @@ export default Ember.Component.extend({
       include: 'order_line_items.line_item'
     });
 
-    promise = promise.sortBy('createdAt');
-
     return promise;
-  }.property('model', 'pastDays', 'showPaid')
+  }.property('model', 'pastDays', 'showPaid'),
+
+
+  nameSort: function() {
+    return this._sortIndicator('userName');
+  }.property('sortProps'),
+
+  paidSort: function() {
+    return this._sortIndicator('paid');
+  }.property('sortProps'),
+
+  paidAmountSort: function() {
+    return this._sortIndicator('paidAmount');
+  }.property('sortProps'),
+
+  netSort: function() {
+    return this._sortIndicator('netAmountReceived');
+  }.property('sortProps'),
+
+  feeSort: function() {
+    return this._sortIndicator('totalFeeAmount');
+  }.property('sortProps'),
+
+  receivedAtSort: function() {
+    return this._sortIndicator('paymentReceivedAt');
+  }.property('sortProps'),
+
+  _sortIndicator: function(field) {
+    let currentSort = this.get('sortProps')[0];
+    let sort = currentSort.split(':');
+    let sortProperty = sort[0];
+    let sortDirection = sort[1];
+
+    if (sortProperty === field) {
+      return (sortDirection === 'desc') ? '▼' : '▲';
+    }
+
+    return '';
+  },
+
+
+  actions: {
+    toggleSort: function(property) {
+      let currentSort = this.get('sortProps')[0];
+      let sort = currentSort.split(':');
+      let sortProperty = sort[0];
+      let sortDirection = sort[1];
+
+      if (property === sortProperty) {
+        sortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+        this.set('sortProps', [property + ':' + sortDirection]);
+      } else {
+        this.set('sortProps', [property + ':asc']);
+      }
+    }
+  }
+
 });
