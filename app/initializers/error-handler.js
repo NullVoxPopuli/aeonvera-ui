4acc05baf13e21e0ceb0c4cc25ecf9f8f953c5b1
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-03-04 23:08:06 -0500

    cleanup

--- app/initializers/error-handler.js
+++ app/initializers/error-handler.js
@@ -3,27 +3,6 @@ import ENV from '../config/environment';
 
 var alreadyRun = false;
 
-var reportError = function (errorData) {
-  Ember.$.ajax({
-    url: 'https://aeonvera.com/api/front_end_error',
-    method: 'POST',
-    dataType: 'json',
-    data: {
-      error: errorData,
-    },
-    success: function (/*data, textStatus, jqXHR*/) {
-      // notify the user what happened, give link
-      // similar to atom.io's editor
-
-    },
-
-    error: function (/*jqXHR, textStatus, errorThrown*/) {
-      // not sure what to do if this fails... we can't report it
-    },
-  });
-
-};
-
 /*
   Adapter Error:
     errorData:
@@ -97,7 +76,7 @@ export default {
 
     Ember.onerror = function (error) {
       Ember.Logger.error(error);
-      displayError(error);
+      // displayError(error);
     };
   },
 };
--- tests/integration/components/date-time-input-test.js
+++ tests/integration/components/date-time-input-test.js
@@ -1,22 +1,15 @@
-import {
-  moduleForComponent, test
-}
-from 'ember-qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 
 import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('date-time-input',
   'Integration | Component | date-time-input', {
-    integration: true,
+    integration: true
   });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-  let two = Ember.Object.extend({
-    someDate: null,
-  });
+test('it renders this month when no date is set', function(assert) {
+  let two = Ember.Object.extend({ someDate: null });
   this.set('two', two);
   this.render(hbs`{{date-time-input model=two field='someDate'}}`);
 
