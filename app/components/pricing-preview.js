7835159469400389d48bf63a8a3da7e88a2cc504
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-03-14 08:14:09 -0400

    Fixes NullVoxPopuli/aeonvera#483 - ticket price preview broke after checking the checkbox

--- app/components/pricing-preview.js
+++ app/components/pricing-preview.js
@@ -1,35 +1,41 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  absorbFees: false,
+  serviceFee: 0,
+  cardFee: 0,
+  buyerPays: 0,
+  youGet: 0,
+  ticketPrice: 0,
+  enteredValue: 75,
+
+  enteredValueChanged: Ember.observer('enteredValue', 'absorbFees', function(){
+    this.send('reCalculate');
+  }),
+
   didInsertElement: function () {
-    this.send('reCalculate', 75);
+    this.send('reCalculate');
     this._super();
   },
 
   actions: {
-    reCalculate: function (enteredValue) {
-      var serviceFee = this.$('#serviceFee');
-      var cardFee = this.$('#cardFee');
-      var buyerPays = this.$('#buyerPays');
-      var youGet = this.$('#youGet');
-      var ticketPrice = this.$('#ticketPrice');
-      var absorbFees = this.$('#absorbFees');
+    reCalculate: function () {
+      let enteredValue = this.get('enteredValue');
+      let absorbFees = this.get('absorbFees');
 
-      var value = typeof enteredValue !== 'undefined' ? enteredValue :
-      parseFloat(
-      ticketPrice.val());
+      var value = parseFloat(enteredValue);
 
       if (isNaN(value)) {
         value = 0;
       }
 
-      var serviceFeeValue = 0;
-      var cardFeeValue = 0;
-      var buyerPaysValue = 0;
-      var youGetValue = 0;
+      let serviceFeeValue = 0;
+      let cardFeeValue = 0;
+      let buyerPaysValue = 0;
+      let youGetValue = 0;
 
       if (value > 0) {
-        if (absorbFees.is(':checked')) {
+        if (absorbFees) {
           serviceFeeValue = value * 0.0075;
           cardFeeValue = value * 0.029 + 0.3;
           buyerPaysValue = value;
@@ -45,11 +51,10 @@ export default Ember.Component.extend({
         }
       }
 
-      serviceFee.html(serviceFeeValue.toFixed(2));
-      cardFee.html(cardFeeValue.toFixed(2));
-      buyerPays.html(buyerPaysValue.toFixed(2));
-      youGet.html(youGetValue.toFixed(2));
-
+      this.set('serviceFee', serviceFeeValue.toFixed(2));
+      this.set('cardFee', cardFeeValue.toFixed(2));
+      this.set('buyerPays', buyerPaysValue.toFixed(2));
+      this.set('youGet', youGetValue.toFixed(2));
     },
   },
 
