602078cc5c31283603af9add990eb208ba738214
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-03-19 08:10:29 -0400

    Fixes NullVoxPopuli/aeonvera#158 -- checkin list now allows teh navigation to attendee summary/detail

--- app/components/attendance-link.js
+++ app/components/attendance-link.js
@@ -1,15 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  id: function () {
+  id: Ember.computed(function () {
     return this.get('model.id');
-  }.property(),
+  }),
 
-  name: function () {
+  eventId: Ember.computed(function(){
+    return this.get('model.eventId');
+  }),
+
+  name: Ember.computed(function () {
     let model = this.get('model');
     let aName = model.get('attendeeName');
     let name = model.get('name');
 
     return aName || name;
-  }.property(),
+  }),
 });
