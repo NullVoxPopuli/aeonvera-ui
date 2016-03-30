59cedcf821e8f47174d5e0482d9576265be6bba8
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-03-30 06:33:08 -0400

    work on table sorting

--- app/components/sortable-table.js
+++ app/components/sortable-table.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  columns: [],
+  data: [],
+
+  didInsertElement(){
+    this._super(...arguments);
+    this._evaluateColumnProperties();
+  },
+
+  _evaluateColumnProperties(){
+    let columns = this.get('columns');
+    let evaluatedColumns = [];
+
+    columns.forEach(column => {
+      if (Ember.isPresent(column.showOn)){
+        let showOn = this.get('model').get(column.showOn);
+        column.showOn = showOn;
+      } else {
+        column.showOn = true;
+      }
+
+      evaluatedColumns.push(column);
+    });
+
+    this.set('columns', evaluatedColumns);
+  }
 });
--- app/controllers/events/show/competitions/show/index.js
+++ app/controllers/events/show/competitions/show/index.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   columns: [
-    {propertyName: 'order.userName', title: 'Name', disableFiltering: true},
-    {propertyName: 'danceOrientation', title: 'Orientation', disableFiltering: true, isHidden: 'model.requiresOrientation'},
-    {propertyName: 'partnerName', title: 'Partner', disableFiltering: true, isHidden: 'model.requiresPartner'},
+    {property: 'order.userName', title: 'Name' },
+    {property: 'danceOrientation', title: 'Orientation', showOn: 'requiresOrientation'},
+    {property: 'partnerName', title: 'Partner', showOn: 'requiresPartner'},
   ]
 });
