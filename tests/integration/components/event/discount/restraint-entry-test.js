8ec1b6347cff73713d1c7d0fcedcf2e0c7fc98b5
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-04-01 14:24:06 -0400

    remove boilerplate test

--- tests/integration/components/event/discount/restraint-entry-test.js
+++ tests/integration/components/event/discount/restraint-entry-test.js
@@ -4,21 +4,3 @@ import hbs from 'htmlbars-inline-precompile';
 moduleForComponent('event/discount/restraint-entry', 'Integration | Component | event/discount/restraint entry', {
   integration: true
 });
-
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });"
-
-  this.render(hbs`{{event/discount/restraint-entry}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#event/discount/restraint-entry}}
-      template block text
-    {{/event/discount/restraint-entry}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
-});
