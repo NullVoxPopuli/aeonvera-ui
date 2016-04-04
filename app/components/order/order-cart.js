ba80790398496625d510a218994618318fd505db
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-04-04 19:14:26 -0400

    work on resizing the cart

--- app/components/order/order-cart.js
+++ app/components/order/order-cart.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+// import ResizeAware from 'ember-resize/mixins/resize-aware';
+import ResizeMixin from 'ember-resize-mixin/main';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(ResizeMixin, {
   cart: Ember.inject.service('order-cart'),
   orderContainerClasses: 'large-4 medium-4 columns fixed-to-top-cart fixed-cart-window-to-small',
   errors: [],
@@ -19,6 +21,38 @@ export default Ember.Component.extend({
       this.set('cart.email', order.get('userEmail'));
     }
   },
+
+  /*
+    Debounced triggers ever 100ms
+
+    This is needed because the cart can grow beyond the bounds of the
+    window. At best, it needs to be slightly smaller than the height,
+    and the tbody needs to be able to scroll.
+
+    the width and height parameters given are the document width and height.
+    for this resize, we need to get the height from the window.
+  */
+  debouncedDidResize: function(){
+    console.log('watwat');
+    let cart = this.$('.fixed-to-top-cart');
+    let windowHeight = this.$(window).height();
+
+    // cart might not be rendered right now
+    if (cart.length === 0){
+      return;
+    }
+
+    let cartTop = cart.position().top;
+    let cartHeight = cart.height();
+    let cartBottom = cartTop + cartHeight;
+    let cartTBody = cart.find('tbody');
+    let cartTBodyHeight = cartTBody.height();
+    let cartUiHeight = cartHeight - cartTBodyHeight;
+    let cartUiHeightWithTop = cartUiHeight + cartTop;
+    let availableHeight = windowHeight - cartUiHeightWithTop;
+
+    cartTBody.css({maxHeight: availableHeight + 'px'});
+  }.on('resize'),
 
   itemContainerClasses: Ember.computed('buildingAnOrder', function() {
     let building = this.get('buildingAnOrder');
