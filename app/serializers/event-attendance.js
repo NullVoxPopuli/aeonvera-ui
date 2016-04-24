b5e30a9f91ed1630e1ac1c385911ba0c805379ee
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-04-24 13:38:51 -0400

    the event attendance will always serialize custom field responses

--- app/serializers/event-attendance.js
+++ app/serializers/event-attendance.js
@@ -4,6 +4,7 @@ import EmbeddedRecords from 'aeonvera/mixins/serializers/embedded-records';
 export default DS.JSONAPISerializer.extend(EmbeddedRecords, {
   attrs: {
     housingRequest: { embedded: 'always' },
-    housingProvision: { embedded: 'always' }
+    housingProvision: { embedded: 'always' },
+    customFieldResponses: { embedded: 'always' }
   }
 });
