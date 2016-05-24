fde80aac6942d48224a02aa80307342a8a453db0
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-05-24 08:38:22 -0400

    delete boilerplate tests

--- tests/integration/pods/components/editing/organization-form/component-test.js
+++ tests/integration/pods/components/editing/organization-form/component-test.js
@@ -4,21 +4,3 @@ import hbs from 'htmlbars-inline-precompile';
 moduleForComponent('editing/organization-form', 'Integration | Component | editing/organization form', {
   integration: true
 });
-
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{editing/organization-form}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#editing/organization-form}}
-      template block text
-    {{/editing/organization-form}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
-});
--- tests/integration/pods/components/form/file-upload/component-test.js
+++ tests/integration/pods/components/form/file-upload/component-test.js
@@ -4,21 +4,3 @@ import hbs from 'htmlbars-inline-precompile';
 moduleForComponent('form/file-upload', 'Integration | Component | form/file upload', {
   integration: true
 });
-
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{form/file-upload}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#form/file-upload}}
-      template block text
-    {{/form/file-upload}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
-});
