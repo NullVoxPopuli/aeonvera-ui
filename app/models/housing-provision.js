37415f9e58d0ef734760714f6a45e1d9b8b57489
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-06-04 17:25:35 -0400

    add defaults for housing provision

--- app/models/housing-provision.js
+++ app/models/housing-provision.js
@@ -5,11 +5,11 @@ import PaymentStatus from '../mixins/models/payment-status';
 export default DS.Model.extend({
   housingCapacity: DS.attr('number', { defaultValue: 0 } ),
   numberOfShowers: DS.attr('number', { defaultValue: 0 } ),
-  canProvideTransportation: DS.attr('boolean'),
+  canProvideTransportation: DS.attr('boolean', { defaultValue: false }),
   transportationCapacity: DS.attr('number', { defaultValue: 0 }),
   preferredGenderToHost: DS.attr('string', { defaultValue: 'No Preference' }),
-  hasPets: DS.attr('boolean'),
-  smokes: DS.attr('boolean'),
+  hasPets: DS.attr('boolean', { defaultValue: false } ),
+  smokes: DS.attr('boolean', { defaultValue: false } ),
   notes: DS.attr('string'),
 
   host: DS.belongsTo('host', { polymorphic: true }),
