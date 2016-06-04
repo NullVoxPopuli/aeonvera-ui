7aa988397cab75a87c7eaf935c4a396414cdc398
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-06-04 17:25:59 -0400

    use ===

--- app/helpers/to-bool.js
+++ app/helpers/to-bool.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Helper.helper(function (params) {
   let value = params[0];
 
-  let boolValue = (!!value || value == 't');
+  let boolValue = (!!value || value === 't');
 
   return boolValue ? 'Yes' : 'No';
 });
