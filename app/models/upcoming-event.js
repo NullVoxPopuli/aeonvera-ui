18632ffdcdccc06f318d81da68620353227d288d
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-06-07 20:35:34 -0400

    add logo to upcoming events and waiting page

--- app/models/upcoming-event.js
+++ app/models/upcoming-event.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
@@ -7,10 +8,19 @@ export default DS.Model.extend({
   endsAt: DS.attr('date'),
   location: DS.attr('string'),
   url: DS.attr('string'),
+  logoUrl: DS.attr('string'),
+  logoUrlThumb: DS.attr('string'),
+  logoUrlMedium: DS.attr('string'),
 
   isRegistrationOpen: function () {
     var opensAt = this.get('registrationOpensAt').getTime();
     var currently = Date.now();
     return (currently > opensAt);
   }.property('registrationOpensAt'),
+
+  logoIsMissing: Ember.computed('logoUrl', function() {
+    let logoUrl = this.get('logoUrl');
+    let logoPresent = Ember.isPresent(logoUrl);
+    return logoPresent ? logoUrl.indexOf('missing') !== -1 : true;
+  }),
 });
