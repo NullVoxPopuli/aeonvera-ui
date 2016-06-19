0e59b232ba6147eea513b87b01c754509a57284a
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-06-19 17:03:04 -0400

    forgot to add files

---
+++ app/pods/register/custom-field-response/component.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  cart: Ember.inject.service('order-cart'),
+
+  response: Ember.computed('attendance', function() {
+      let attendance = this.get('attendance');
+
+      if (attendance != null) {
+
+        let field = this.get('field');
+        let existingCustomField = attendance.get('customFieldResponses').find(item =>
+          field.get('id') === item.get('customField.id'));
+
+        if (Ember.isPresent(existingCustomField)) {
+
+          // if datetime...
+          let kind = field.get('kind');
+
+          // if (kind >= 3 && kind <= 5){
+          //   // convert the string value to time
+          //   // let value = existingCustomField.get('value');
+          //   // existingCustomField.set('value', moment(value).format('lll'));
+          // }
+          return existingCustomField;
+        }
+
+        let response = this.get('store').createRecord('custom-field-response', {
+          value: this.get('field.defaultValue'),
+          customField: field,
+          writer: this.get('attendance')
+        });
+
+        attendance.get('customFieldResponses').pushObject(response);
+
+        return response;
+      }
+    }),
+
+  actions: {
+
+  }
+});
