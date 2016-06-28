75522087b56c1fb375c2ead2f8f08e00c6f95ba8
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-06-28 11:16:52 -0400

    upgrade ember, ember-cli

--- app/pods/register/checkout/route.js
+++ app/pods/register/checkout/route.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 import ResetScroll from 'aeonvera/mixins/routes/reset-scroll';
 
 /*jshint multistr: true */
-const includeString = '\
-attendance,\
-attendance.custom_field_responses,attendance.housing_request,\
-attendance.housing_provision,host,\
-order_line_items.line_item.restraints,host.integrations';
+const includeString = `
+attendance,
+attendance.custom_field_responses,attendance.housing_request,
+attendance.housing_provision,host,
+order_line_items.line_item.restraints,host.integrations`;
 
 export default Ember.Route.extend(ResetScroll, {
 
@@ -14,7 +14,7 @@ export default Ember.Route.extend(ResetScroll, {
     return this.get('store').findRecord('order', params.orderId, {
       adapterOptions: {
         query: {
-          include: includeString
+          include: ''
         }
       }
     });
