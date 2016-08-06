33dd3101a06daee99e4baf7b470a3fea09e03d4f
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-08-06 15:54:21 -0400

    marking paid works for cash

--- app/pods/components/order/mark-paid-modal/component.js
+++ app/pods/components/order/mark-paid-modal/component.js
@@ -1,23 +1,25 @@
 import Ember from 'ember';
-import ENV from 'aeonvera/config/environment';
 
 const { computed } = Ember;
 
 export default Ember.Component.extend({
+  ajax: Ember.inject.service('authenticated-ajax'),
+
   // passed in
   order: null,
 
   // set in template via radio buttons
   paymentMethods: {
     // PAYPAL: "PayPal",
-    CHECK: "Check",
+    CHECK: 'Check',
+
     // CREDIT: "Credit",
     // DEBIT: "Debit",
-    CASH: "Cash",
-    STRIPE: "Stripe"
+    CASH: 'Cash',
+    STRIPE: 'Stripe'
   },
   paymentMethod: 'Cash',
-  cashOrCheckAmount: computed.oneWay('model.total'),
+  cashOrCheckAmount: null,
   checkNumber: '',
 
   modalName: computed('order', {
@@ -26,7 +28,23 @@ export default Ember.Component.extend({
 
   actions: {
     markPaid() {
+      let id = this.get('order.id');
+      let url = '/api/orders/' + id  + '/mark_paid';
+      let data = {
+        payment_method: this.get('paymentMethod'),
+        amount: this.get('cashOrCheckAmount'),
+        check_number: this.get('checkNumber')
+      };
 
+      this.get('ajax').PUT(url, data).then(data => {
+        this.get('store').pushPayload(data);
+        this.get('flashMessages').success('Order was successfully marked as paid.');
+        Ember.$('.close-reveal-modal').click();
+      }, error => {
+        let json = JSON.parse(error.responseText);
+        let errors = json.errors;
+        this.get('flashMessages').alert(errors);
+      });
     }
   }
 });
---
+++ app/services/authenticated-ajax.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+import ENV from 'aeonvera/config/environment';
+
+export default Ember.Service.extend({
+  session: Ember.inject.service(),
+
+  GET(path) {
+    return this._perform('GET', path, {});
+  },
+
+  POST(path, data) {
+    return this._perform('POST', path, data);
+  },
+
+  PUT(path, data) {
+    return this._perform('PUT', path, data);
+  },
+
+  _perform(method, path, data) {
+    let url = ENV.host + path;
+    let authToken = this.get('session.data.authenticated.token');
+
+    return Ember.$.ajax({
+      url: url,
+      type: method,
+      data: data,
+      beforeSend(xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer ' + authToken);
+      }
+    });
+  }
+});
