05acdf3a05e596829e3bba87bc6b7bbe0f3a6a3e
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-09-03 12:39:19 -0400

    change orders screen to have currentPaidAmount

--- app/pods/events/show/orders/index/controller.js
+++ app/pods/events/show/orders/index/controller.js
@@ -4,7 +4,7 @@ import ENV from 'aeonvera/config/environment';
 export default Ember.Controller.extend({
   columns: [
     { property: 'userName', title: 'Name' },
-    { property: 'total', title: 'Total' },
+    { property: 'currentPaidAmount', title: 'Paid Amount' },
     { property: 'paid', title: 'Paid' },
     { property: 'createdAt', title: 'Created At' },
     { property: '', title: 'Registration', sort: false }
