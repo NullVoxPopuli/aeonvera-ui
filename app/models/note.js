ca58d374df91e3bb35b7114896f66020d18e6672
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-12-04 14:28:37 -0500

    begin adding notes, and fix the membership buttons

---
+++ app/models/note.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+import DeletedAt from 'aeonvera/mixins/models/deleted-at';
+
+const { Model, attr, belongsTo } = DS;
+
+export default Model.extend(DeletedAt, {
+  note: attr('string'),
+
+  author: belongsTo('user'),
+  host:   belongsTo('host', { polymorphic: true }),
+  target: belongsTo('user', { polymorphic: true })
+});
