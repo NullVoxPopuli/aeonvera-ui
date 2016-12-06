48eaff16805c583309ad5239f19c118c93762e8f
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-12-05 22:41:43 -0500

    fix some bugs

--- tests/unit/helpers/as-hash-test.js
+++ tests/unit/helpers/as-hash-test.js
@@ -5,6 +5,6 @@ module('Unit | Helper | as hash');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = asHash([42]);
-  assert.ok(result);
+  let result = asHash({}, { a: 1, b: 2 });
+  assert.equal(result, { a: 1, b: 2 });
 });
--- tests/unit/helpers/user/latest-renewal-for-test.js
+++ tests/unit/helpers/user/latest-renewal-for-test.js
@@ -2,9 +2,3 @@ import { userLatestRenewalFor } from 'aeonvera/helpers/user/latest-renewal-for';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | user/latest renewal for');
-
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = userLatestRenewalFor([42]);
-  assert.ok(result);
-});
