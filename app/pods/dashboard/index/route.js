8e1adb3180070873231b53645accdedd3693680f
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-06-24 13:33:29 -0400

    fix dashboard return title

--- app/pods/dashboard/index/route.js
+++ app/pods/dashboard/index/route.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
+import SetNavBarTitle from 'aeonvera/mixins/routes/set-navbar-title';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(SetNavBarTitle, {
+  i18n: Ember.inject.service(),
+
   model: function() {
     return this.store.findAll('upcoming-event');
+  },
+
+  afterModel() {
+    this._setAppNavTitle(this.get('i18n').t('appname'));
   }
 });
