7df34ab4d91d1ae18010a6c48f5aa53e137b868c
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-06-24 13:09:12 -0400

    remove partials

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -x
-
+echo $DEPLOY_TARGET
 docker-compose -f docker-compose.test.ci.yml run \
   --rm test-server bash -c "\
     yarn install && bower install && \
     DEPLOY_ENV=$DEPLOY_TARGET ember deploy $DEPLOY_TARGET --activate \
     "
-
