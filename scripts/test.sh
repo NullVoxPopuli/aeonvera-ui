efc94e88d60db2097a02160a2e844173e4bdbfd5
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-06-27 09:03:06 -0400

    update ci

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ set -x
 # docker-compose -f docker-compose.test.yml rm --all -f
 docker-compose -f docker-compose.test.ci.yml build
 docker-compose -f docker-compose.test.ci.yml run --rm test-server npm install && bower install
-docker-compose -f docker-compose.test.ci.yml run --rm test-server
+docker-compose -f docker-compose.test.ci.yml run --rm test-server ember test --server --test-port 4310 --line-reload-port 4311
