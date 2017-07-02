100a42d29a0e14aa22b95cf48b2ae046605b01f9
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-07-02 09:02:56 -0400

    inadvertently fixed a double render error

--- app/pods/register/event-registration/index/route.js
+++ app/pods/register/event-registration/index/route.js
@@ -5,12 +5,19 @@ import { UNREGISTERED_ID } from 'aeonvera/models/registration';
 
 const { isEmpty } = Ember;
 
+const include = `
+orders.order_line_items.line_item
+unpaid_order.order_line_items.line_item
+housing_request
+housing_provision
+`.split('\n').join(',');
+
 export default Ember.Route.extend({
   model() {
     const event = this.modelFor('register.event-registration');
 
     const registrations = this.store.query('registration', {
-      include: 'order.order_line_items,unpaid_order.order_line_items',
+      include: include,
       q: {
         host_id_eq: event.get('id'),
         host_type_eq: 'Event'
