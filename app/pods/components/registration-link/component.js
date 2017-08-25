67f5c985c95aa3e043fe15beb2445f9e10c862c5
Preston Sego <LPSego3+dev@gmail.com>
2017-08-24 22:52:47 -0400

    fixed registration-link

--- app/pods/components/registration-link/component.js
+++ app/pods/components/registration-link/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 import { alias } from 'ember-decorators/object/computed';
 
 export default class extends Ember.Component {
-  @alias('model.id') id;
-  @alias('model.eventId') eventId;
+  @alias('model.id') registrationId;
+  @alias('model.host.id') eventId;
   @alias('model.name') name;
 }
