a94f9f7e6423efcf9f54fc789fc7a81b67c5d511
Preston Sego <LPSego3+dev@gmail.com>
2017-08-31 07:19:52 -0400

    more ts

--- tests/helpers/authenticate-session.ts
+++ tests/helpers/authenticate-session.ts
@@ -4,6 +4,10 @@ import {
   authenticateSession
 } from 'aeonvera/tests/helpers/ember-simple-auth';
 
+declare global {
+  export const authenticateSession: typeof authenticateSession;
+}
+
 export default Ember.Test.registerAsyncHelper('authenticateSession', function(app, data) {
   authenticateSession(app, data);
 });
