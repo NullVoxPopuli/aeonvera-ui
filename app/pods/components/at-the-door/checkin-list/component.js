0ca3bb5690a0e229f97f8b41adfb25ab60167a5f
Preston Sego <LPSego3+dev@gmail.com>
2017-09-19 19:02:31 -0400

    pagination!

--- app/pods/components/at-the-door/checkin-list/component.js
+++ app/pods/components/at-the-door/checkin-list/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
-import { computed } from 'ember-decorators/object';
+import { computed, action } from 'ember-decorators/object';
+import { sort, alias } from 'ember-decorators/object/computed';
 
 export default class extends Ember.Component {
   queryText = '';
@@ -8,6 +9,67 @@ export default class extends Ember.Component {
   showOnlyThoseWhoOweMoney = false;
 
   activeRegistrant = null;
+
+  limit = 5;
+
+  @alias('registrations.length') numberOfRegistrations;
+
+  @computed('numberOfRegistrations', 'limit')
+  numberOfPages(num, limit) {
+    return num / limit;
+  }
+
+  @computed('numberOfPages')
+  pages(num) {
+    let result = [];
+
+    for (let i = 0; i < num; i++) {
+      result.push(i + 1);
+    }
+
+    return result;
+  }
+  // pages = Ember.A([1, 2, 3]);
+  limitOptions = Ember.A([10, 20, 30]);
+  page = 1;
+  sortProp = 'name';
+  sortDir = 'asc';
+
+  @computed('sortProp', 'sortDir')
+  sortProperty(prop, dir) {
+    return [`${prop}:${dir}`];
+  }
+
+  @computed('sortedRegistrations', 'page', 'limit')
+  paginatedRegistrations(registrations, page, limit) {
+    let ind = (page - 1) * limit;
+
+    return Ember.A(registrations.toArray().splice(ind, limit));
+  }
+
+  @sort('registrations', 'sortProperty') sortedRegistrations;
+
+  @action
+  decrementPage() {
+    let page = this.get('page');
+    if (page > 0) {
+      this.set('page',page-1);
+    }
+  }
+
+  @action
+  changeSort(args) {
+    console.log(args, ...arguments);
+  }
+
+  @action
+  incrementPage() {
+    let page =  this.get('page');
+    let max = this.get('pages').reduce((prev,curr) => curr > prev ? curr : prev,0);
+    if (page < max) {
+      this.set('page',page+1);
+    }
+  }
 
   @computed('model', 'queryText', 'showOnlyNonCheckedIn', 'showOnlyThoseWhoOweMoney')
   registrations(model, query, onlyNonCheckedIn, onlyOweMoney) {
@@ -59,12 +121,14 @@ export default class extends Ember.Component {
     return model.filterBy('isCheckedIn', false).get('length');
   }
 
-  actions = {
-    setActiveRegistrant(registration) {
-      this.set('activeRegistrant', registration);
-    },
-    loadNext() {
-     this.get('pagedRegistrations').loadNextPage();
-   }
+  @action
+  setActiveRegistrant(registration) {
+    this.set('activeRegistrant', registration);
   }
+
+  @action
+  loadNext() {
+    this.get('pagedRegistrations').loadNextPage();
+  }
+
 }
