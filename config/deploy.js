9ffbb2033ebc9a6a0cb18b2bba5273f55c540292
Preston Sego <LPSego3+dev@gmail.com>
2018-02-07 08:52:43 -0500

    deployment adjustments

--- config/deploy.js
+++ config/deploy.js
@@ -1,6 +1,8 @@
 /* jshint node: true */
 
 module.exports = function(deployTarget) {
+  var bucketSuffix = deployTarget === 'development' ? 'dev' : deployTarget;
+
   var ENV = {
     build: {},
     /* upload assets */
@@ -8,16 +10,22 @@ module.exports = function(deployTarget) {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: 'us-east-1',
-      /* folder */
       prefix: 'ember'
     },
     /* upload index.html */
-    redis: {
-      host: 'localhost',
-      port: '6379',
-      password: '',
-      allowOverwrite: true
+    ['s3-index']: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      allowOverwrite: true,
+      region: 'us-east-1',
+      prefix: 'ember'
     },
+    // redis: {
+    //   host: 'localhost',
+    //   port: '6379',
+    //   password: '',
+    //   allowOverwrite: true
+    // },
 
     gzip: {
       /* default, minus map */
@@ -28,36 +36,17 @@ module.exports = function(deployTarget) {
       accessToken: 'ca10480ec923459abdbe39a95c1181d9',
       accessServerToken: 'ecd51b9e05084953bf2f9bc1e7dd3e77',
       minifiedPrependUrl: function(context){
-        return ['https://s3.amazonaws.com/aeonvera-' + deployTarget + '/ember/'];
+        return ['https://s3.amazonaws.com/aeonvera-' + bucketSuffix + '/ember/'];
       },
       environment: deployTarget,
       username: process.env.USER + '@' + process.env.HOSTNAME
     }
   };
 
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    ENV.s3.bucket = 'aeonvera-dev';
-    // configure other plugins for development deploy target here
-  }
-
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'staging';
-    // configure other plugins for staging deploy target here
-    ENV.s3.bucket = 'aeonvera-staging';
-    ENV.redis.host = process.env.REDISCLOUD_URL;
-    ENV.redis.port = process.env.REDISCLOUD_PORT;
-    ENV.redis.password = process.env.REDISCLOUD_PASSWORD;
-  }
-
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
-    ENV.s3.bucket = 'aeonvera-production';
-    ENV.redis.host = process.env.REDISCLOUD_URL;
-    ENV.redis.port = process.env.REDISCLOUD_PORT;
-    ENV.redis.password = process.env.REDISCLOUD_PASSWORD;
-  }
+  // development, staging, production
+  ENV.build.environment = deployTarget;
+  ENV.s3.bucket = `aeonvera-${bucketSuffix}`;
+  ENV['s3-index'].bucket = ENV.s3.bucket;
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
---
+++ tests/.types.d.ts
@@ -0,0 +1,14 @@
+// declare module Ember {
+//   module $ {
+//     export interface mockjax: MockJaxStatic;
+//   }
+// }
+
+/// <reference types="jquery-mockjax" />
+
+//
+// namespace Ember {
+//   namespace $ {
+//     export function mockjax: MockJaxStatic
+//   }
+// }
