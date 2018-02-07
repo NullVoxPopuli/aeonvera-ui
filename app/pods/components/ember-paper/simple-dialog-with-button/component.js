c54c1516d3131fa93a8dbe1951d77bddea4e2770
NullVoxPopuli <lpsego3+dev@gmail.com>
2018-02-07 02:41:06 +0000

    Cleanup?

--- app/pods/components/ember-paper/simple-dialog-with-button/component.js
+++ app/pods/components/ember-paper/simple-dialog-with-button/component.js
@@ -1,12 +1,32 @@
-import Ember from 'ember';
+import Component from '@ember/component';
+import { PropTypes } from 'ember-prop-types';
 
-export default Ember.Component.extend({
-  tagName: '',
-  raised: true,
+import { action } from 'ember-decorators/object';
 
-  actions: {
-    closeModal() {
-      this.set('showModal', false);
-    }
+export default class extends Component {
+  // tagName = '';
+  raised = true;
+
+  @action
+  closeModal() {
+    this.set('showModal', false);
   }
-});
+
+  @action
+  async onClick() {
+    const onClick = this.get('onClick');
+
+    if (onClick) {
+      try {
+        const result = await onClick();
+        console.log('onClick: ', result);
+        if (!result) return;
+
+        this.set('showModal', true);
+      } catch(e) { console.error(e); }
+    } else {
+      this.set('showModal', true);
+    }
+
+  }
+}
--- app/pods/register/route.js
+++ app/pods/register/route.js
@@ -1,46 +1,45 @@
 import Ember from 'ember';
+import { action } from 'ember-decorators/object';
+import { service } from 'ember-decorators/service'
 
-export default Ember.Route.extend({
-  model: function(params) {
+export default class extends Ember.Route {
+  @service('flash-notification') flash;
+  model(params) {
     const subdomain = params.subdomain;
 
     return this.get('store').findRecord('host', subdomain);
-  },
+  }
 
-  actions: {
-    eerror(reason, transition) {
-      if (reason.errors === undefined) {
-        // This is a non-API error
-        console.error(reason);
-        console.error(transition);
-        return;
-      }
+  @action
+  error(reason, transition) {
+    const flash = this.get('flash');
+    if (reason.errors === undefined) {
+      this._super(...arguments);
+    }
 
-      const firstError = reason.errors[0];
-      const errorObject = new Ember.Object(firstError);
-      const status = firstError.code;
+    const firstError = reason.errors[0];
+    const errorObject = new Ember.Object(firstError);
+    const status = firstError.code;
 
-      // all errors are json api-formatted
-      if (status === 404) {
-        const controller = errorObject.get('meta.params.controller');
+    // all errors are json api-formatted
+    if (status === 404) {
+      const controller = errorObject.get('meta.params.controller');
 
-        if (controller && controller.includes('orders')) {
-          // the order was not found, transition to the main
-          // register route
-          const subdomain = transition.params.register.subdomain;
-
-          transition.abort();
-          return this.transitionTo('register', subdomain);
-        }
+      if (controller && controller.includes('orders')) {
+        // the order was not found, transition to the main
+        // register route
+        const subdomain = transition.params.register.subdomain;
 
         transition.abort();
-        return this.transitionTo('event-not-found');
+        flash.error('order not found');
+        return this.transitionTo('register', subdomain);
       }
 
-      // panic?
-      console.error(reason);
-      alert('a registration error has occurred, please notify support');
-
+      transition.abort();
+      flash.error('Event not found');
+      return this.transitionTo('event-not-found');
     }
+
+    this._super(...arguments);
   }
-});
+}
