f8bab0db8021e0cd8b10a43e74930dfbee3884e5
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-02-16 07:21:44 -0500

    clean up footer a bit

--- app/components/links/facebook-icon-link.js
+++
@@ -1,7 +0,0 @@
-import ExternalLink from 'aeonvera/components/links/external-link';
-
-export default ExternalLink.extend({
-  layoutName: 'components/links/external-link',
-  href: 'https://www.facebook.com/aeonvera',
-  icon: 'facebook-official'
-});
--- app/components/links/mail-support-icon-link.js
+++
@@ -1,7 +0,0 @@
-import ExternalLink from 'aeonvera/components/links/external-link';
-
-export default ExternalLink.extend({
-  layoutName: 'components/links/external-link',
-  href: 'mailto:support@aeonvera.com',
-  icon: 'envelope'
-});
