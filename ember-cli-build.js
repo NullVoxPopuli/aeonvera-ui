1671652436210d428b7872afd840911a2ee87514
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-02-16 06:41:22 -0500

    I did it. yay. local font awesome

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,16 @@
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
 var env = EmberApp.env();
 var isProductionLike = ['production', 'staging'].indexOf(env) > -1;
 
-
 module.exports = function(defaults) {
-  var fingerprintPrepend = '';
-  console.log(env);
-  switch(env){
-    case 'staging':
-      fingerprintPrepend = 'https://s3.amazonaws.com/aeonvera-staging/ember/';
-    break;
-    case 'production':
-      fingerprintPrepend = 'https://s3.amazonaws.com/aeonvera-production/ember/';
-    break;
-  };
+  var fingerprintPrepend = `https://s3.amazonaws.com/aeonvera-${env}/ember/`;
 
-  console.log(fingerprintPrepend);
+  console.log(env, fingerprintPrepend);
 
   let app = new EmberApp(defaults, {
     babel: {
@@ -44,26 +37,18 @@ module.exports = function(defaults) {
     hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLike
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
   // countdowns! (In human-readable format)
   app.import('vendor/countdown.js');
+
+  // font awesome
+  app.import('vendor/fontawesome/css/font-awesome-all.min.css');
+  var fontTree = new Funnel('vendor/fontawesome/webfonts', { destDir: '/assets/fontawesome/webfonts' });
+  var fontStyleTree = new Funnel('vendor/fontawesome/css', { destDir: '/assets/fontawesome/css' });
 
   if (isProductionLike) {
     app.import('vendor/support/crisp.js');
     app.import('vendor/support/fullstory.js');
   }
 
-  return app.toTree();
+  return mergeTrees([app.toTree(), fontTree, fontStyleTree]);
 };
