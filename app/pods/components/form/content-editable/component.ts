b7ed0e271675a399af5a4ff82bd4f4f391d98610
Preston Sego <LPSego3+dev@gmail.com>
2018-02-23 07:13:50 -0500

    Content editable is hard, I guess

---
+++ app/pods/components/form/content-editable/component.ts
@@ -0,0 +1,40 @@
+import Component from '@ember/component';
+import { observer } from '@ember/object';
+
+import { computed, readonly } from '@ember-decorators/object';
+import { alias, not } from '@ember-decorators/object/computed';
+// import { argument } from '@ember-decorators/argument';
+
+export default class FormContentEditable extends Component.extend({
+  // anything which *must* be merged to prototype here
+}) {
+  tagName: string;
+  placeholder: string;
+  disabled = false;
+
+  @readonly
+  value: string;
+
+  classNames = ['ember-content-editable']
+  attributeBindings = ['contenteditable', 'placeholder', 'readonly', 'disabled'],
+
+  spellcheck = false;
+  clearPlaceholderOnFocus = false;
+  allowNewLines = false;
+  type = 'text';
+
+
+  @not('disabled') contenteditable;
+
+
+  valueObserver = observer('value', function() {
+    console.log(this.element);
+    this.element.innerHTML = this.get('value');
+  })
+
+  keyUp(e) {
+    const text = e.target.outerText;
+
+    this.sendAction('onChange', text);
+  }
+};
