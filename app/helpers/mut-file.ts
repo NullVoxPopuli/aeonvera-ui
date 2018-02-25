874c52f679a479ef6ba732ae2e836841fa6df98e
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-02-25 18:23:48 -0500

    eh

--- app/helpers/mut-file.ts
+++ app/helpers/mut-file.ts
@@ -1,22 +1,21 @@
 import { helper } from '@ember/component/helper';
 
 export function mutFile(params, hash) {
-  console.log('[mutFile] ', this, ...arguments);
-  return (...args) => {
-    const value = args[0];
-    console.log('args', args)
-  }
-  //
-  // const file = e.target.files[0];
-  //
-  // const fileName = file.name;
-  // const fileSize = file.size;
-  //
-  // model.set(property + 'FileName', fileName);
-  // model.set(property + 'FileSize', fileSize);
-  // model.get(property).update(file);
-  // model.send('becomeDirty');
+  const [model, fileFieldBaseName, ..._others] = params;
 
+  return (...args) => {
+    const file = args[0];
+
+    const fileName = file.name;
+    const fileSize = file.size;
+
+    model.set(`${fileFieldBaseName}FileName`, fileName);
+    model.set(`${fileFieldBaseName}FileSize`, fileSize);
+
+    model.get(fileFieldBaseName).update(file);
+    model.send('becomeDirty');
+
+  }
   // return params;
 }
 
